f25dfe290b7e65effe06adb8f7840b98b27c9fff
Chris Maikos <chmaikos@gmail.com>
2023-09-20 18:52:50 +0300

    [feat]: Added new gitignore

--- CoperDBAPI/ProducerWind/producerWind.py
+++ CoperDBAPI/ProducerWind/producerWind.py
@@ -1,225 +1,92 @@
 import os
-from netCDF4 import Dataset
-from netCDF4 import num2date
+import logging
+from netCDF4 import Dataset, num2date
 import numpy as np
 import pandas as pd
 import cdsapi
 import configparser
 from datetime import datetime, timedelta
 import time
-import numpy.ma as ma
-from math import pi
+from math import pi, radians, cos
 from confluent_kafka import Producer
 import json
 import pymongo
-from math import radians, cos
 
+# Configure logging
+logging.basicConfig(level=logging.INFO, filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+console = logging.StreamHandler()
+console.setLevel(logging.INFO)
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+console.setFormatter(formatter)
+logging.getLogger('').addHandler(console)
 
 def create_square(latitude, longitude, radius):
-    # Earth's radius in kilometers
     earth_radius = 6371.0
-
-    # Convert latitude and longitude from degrees to radians
     lat_rad = radians(latitude)
     lon_rad = radians(longitude)
-
-    # Convert radius from kilometers to degrees
     radius_deg = radius / earth_radius
-
-    # Calculate the minimum and maximum latitude and longitude
     min_latitude = latitude - radius_deg
     max_latitude = latitude + radius_deg
     min_longitude = longitude - (radius_deg / cos(lat_rad))
     max_longitude = longitude + (radius_deg / cos(lat_rad))
-
     return min_latitude, min_longitude, max_latitude, max_longitude
 
-
-# Create the Kafka producer
-producer = Producer({
-    'bootstrap.servers': 'kafka1:29092'
-})
-
-# Get the list of topics
+producer = Producer({'bootstrap.servers': 'kafka1:29092'})
 topic_metadata = producer.list_topics()
 topic_list = topic_metadata.topics
-
-# Print the topics
 for topic in topic_list:
-    print("-----------------------------------------------------------------------------------------", topic)
-
+    logging.info("----------------------------------------------------------------------------------------- %s", topic)
 topic = 'wind_topic'
-# data = {'message': 'Hello, Kafka!'}
-
 
 def delivery_report(err, msg):
     if err is not None:
-        print(f'Failed to deliver message: {err}')
+        logging.error('Failed to deliver message: %s', err)
     else:
-        print(f'Message delivered to topic: {msg.topic()}')
-
+        logging.info('Message delivered to topic: %s', msg.topic())
 
 myclient = pymongo.MongoClient("mongodb://mongodb:27017")
 db = myclient["kafka_db"]
 mycol = db["windData"]
 
 while True:
-    config = configparser.ConfigParser()
-    config.read('config.conf')
-
-    lon = float(config.get('Default', 'longitude'))
-    lat = float(config.get('Default', 'latitude'))
-    rad = float(config.get('Default', 'radius'))
-
-    # longitude_min = lon - rad
-    # longitude_max = lon + rad
-    #
-    # latitude_min = lat - rad
-    # latitude_max = lat + rad
-
-    latitude_min, longitude_min, latitude_max, longitude_max = create_square(lat, lon, rad)
-
-    # while True:
-    # Get the current time
-    current_time = datetime.now()
-    current_time = current_time - timedelta(days=6)
-
-    # Extract month, day, and time components
-    month_current = current_time.strftime("%m")
-    day_current = current_time.strftime("%d")  # Day of the month (zero-padded)
-    time_current = current_time.strftime("%H:%M")  # Time in HH:MM format
-
-    # Retrieve Wind data from Copernicus C3S and store as netCDF4 file using CDS API
-    c = cdsapi.Client()
-
-    windData = 'data/ERA5_Wind3H.nc'
-    # windData_Feb_2022 = 'data/ERA5_Wind3H.nc'
-
-    dataList = [
-        {'fileLocation': windData, 'month': month_current, 'day': day_current}]
-    print("month", dataList[0]["month"])
-    print("day", dataList[0]["day"])
-    for item in dataList:
-        c.retrieve(
-            'reanalysis-era5-single-levels',
-            {
-                'product_type': 'reanalysis',
-                'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind'],
-                'year': '2023',
-                'month': item['month'],
-                'day': item['day'],
-                'time': [
-                    '00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
-                    '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
-                    '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
-                    '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
-                ],
-                'area': [latitude_max, longitude_min, latitude_min, longitude_max],
-                'format': 'netcdf',
-            },
-            item['fileLocation'])
-
-    windData = Dataset('data/ERA5_Wind3H.nc', 'r+')
-
-    windData_BL = windData
-    # Extract variables for combined wind/wave dataset
-    u10 = windData_BL.variables['u10']
-    v10 = windData_BL.variables['v10']
-
-    # Define the new variable --> wind speed
-    wind_speed = windData_BL.createVariable('wind_speed', np.int16, ('time', 'latitude', 'longitude'))
-    wind_speed.units = 'm s**-1'
-    wind_speed.long_name = '10 metre wind speed'
-
-    # Calculate the square of u10, v10 and write to u10, v10 variable in netCDF4 file
-    u10sq = u10[:] ** 2
-    v10sq = v10[:] ** 2
-
-    # Calculate wind speed from u,v components
-    wind_speed = ma.sqrt(u10sq + v10sq)
-    wind_speed[:]
-
-    # Define the new variable --> wind direction
-    wind_dir = windData_BL.createVariable('wind_dir', np.int16, ('time', 'latitude', 'longitude'))
-    wind_dir.units = 'deg'
-    wind_dir.long_name = '10 metre wind direction'
-
-    # Calculate wind direction from u,v components
-    wind_dir = (270 - np.arctan2(v10[:], u10[:]) * 180 / pi) % 360
-    wind_dir[:]
-
-    # convert time dimension to string
-    nctime = windData_BL.variables['time'][:]  # get values
-    t_unit = windData_BL.variables['time'].units  # get unit  "days since 1950-01-01T00:00:00Z"
-    t_cal = windData_BL.variables['time'].calendar
-    tvalue = num2date(nctime, units=t_unit, calendar=t_cal)
-    str_time = [i.strftime("%Y-%m-%d %H:%M:%S") for i in tvalue]  # to display dates as string
-
-
-    # Get dimensions assuming 3D: time, latitude, longitude
-    time_dim, lat_dim, lon_dim = u10.get_dims()
-    time_var = windData_BL.variables[time_dim.name]
-    times = num2date(time_var[:], time_var.units)
-    latitudes = windData_BL.variables[lat_dim.name][:]
-    longitudes = windData_BL.variables[lon_dim.name][:]
-
-    times_grid, latitudes_grid, longitudes_grid = [
-        x.flatten() for x in np.meshgrid(times, latitudes, longitudes, indexing='ij')]
-
-    df = pd.DataFrame({
-        'time': [t.isoformat(sep=" ") for t in times_grid],
-        'latitude': latitudes_grid,
-        'longitude': longitudes_grid,
-        'u10': u10[:].flatten(),
-        'v10': v10[:].flatten(),
-        'speed': wind_speed[:].flatten(),
-        'direction': wind_dir[:].flatten()
-    })
-
-    print(df)
-
-    # Find null values in 'Feature1'
-    null_values = df['u10'].isnull()
-
-    # Filter the DataFrame to show only rows with null values in 'Feature1'
-    rows_with_null = df[null_values]
-
-    # Print the rows with null values
-    print(rows_with_null)
-
-    # Drop rows with null values in 'u10'
-    df = df.dropna(subset=['u10'])
-
-    print(df)
-
-    # Convert DataFrame to a list of dictionaries (JSON-like documents)
-    data = df.to_dict(orient='records')
-
-    # Insert the data into MongoDB
-    mycol.insert_many(data)
-
-    # Optional: Close the MongoDB connection
-    myclient.close()
-
-    for index, row in df.iterrows():
-
-        data_topic = row.to_dict()
-        # # Convert dictionary to JSON bytes
-        value = json.dumps(data_topic).encode('utf-8')
-
-        producer.produce(topic=topic, value=value, callback=delivery_report)
-
-        producer.flush()
-
-    file_path = 'data/ERA5_Wind3H.nc'
-
     try:
-        os.remove(file_path)
-        print("File deleted successfully.")
+        config = configparser.ConfigParser()
+        config.read('config.conf')
+        lon, lat, rad = map(float, [config.get('Default', 'longitude'), config.get('Default', 'latitude'), config.get('Default', 'radius')])
+        latitude_min, longitude_min, latitude_max, longitude_max = create_square(lat, lon, rad)
+        current_time = datetime.now() - timedelta(days=6)
+        month_current, day_current = current_time.strftime("%m"), current_time.strftime("%d")
+        c = cdsapi.Client()
+        windData = 'data/ERA5_Wind3H.nc'
+        dataList = [{'fileLocation': windData, 'month': month_current, 'day': day_current}]
+        logging.info("month %s", dataList[0]["month"])
+        logging.info("day %s", dataList[0]["day"])
+        for item in dataList:
+            c.retrieve('reanalysis-era5-single-levels', {'product_type': 'reanalysis', 'variable': ['10m_u_component_of_wind', '10m_v_component_of_wind'], 'year': '2023', 'month': item['month'], 'day': item['day'], 'time': [f'{i:02d}:00' for i in range(24)], 'area': [latitude_max, longitude_min, latitude_min, longitude_max], 'format': 'netcdf'}, item['fileLocation'])
+        with Dataset('data/ERA5_Wind3H.nc', 'r+') as windData_BL:
+            u10, v10 = map(windData_BL.variables.get, ['u10', 'v10'])
+            wind_speed = np.sqrt(u10[:]**2 + v10[:]**2)
+            wind_dir = (270 - np.arctan2(v10[:], u10[:]) * 180 / pi) % 360
+            time_dim, lat_dim, lon_dim = u10.get_dims()
+            time_var = windData_BL.variables[time_dim.name]
+            times = num2date(time_var[:], time_var.units)
+            latitudes = windData_BL.variables[lat_dim.name][:]
+            longitudes = windData_BL.variables[lon_dim.name][:]
+            times_grid, latitudes_grid, longitudes_grid = [x.flatten() for x in np.meshgrid(times, latitudes, longitudes, indexing='ij')]
+            df = pd.DataFrame({'time': [t.isoformat(sep=" ") for t in times_grid], 'latitude': latitudes_grid, 'longitude': longitudes_grid, 'u10': u10[:].flatten(), 'v10': v10[:].flatten(), 'speed': wind_speed.flatten(), 'direction': wind_dir.flatten()})
+            logging.info(df)
+            df.dropna(subset=['u10'], inplace=True)
+            logging.info(df)
+            data = df.to_dict(orient='records')
+            mycol.insert_many(data)
+        for index, row in df.iterrows():
+            value = json.dumps(row.to_dict()).encode('utf-8')
+            producer.produce(topic=topic, value=value, callback=delivery_report)
+            producer.flush()
+        os.remove(windData)
+        logging.info("File deleted successfully.")
     except FileNotFoundError:
-        print("File not found.")
+        logging.error("File not found.")
     except Exception as e:
-        print(f"Error: {e}")
-
-    # Sleep for 1 day
+        logging.error("Error: %s", e)
     time.sleep(24 * 60 * 60)
--- README.md
+++ README.md
@@ -1,60 +1,47 @@
 # CoperDBAPI
 
-<p align="center">
-  <img width="870" height="700" src="https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-24%20at%203.33.34%20PM.png" alt="CoperDBAPI"/>
-</p>
+![CoperDBAPI](https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-24%20at%203.33.34%20PM.png)
 
+- [CoperDBAPI](#coperdbapi)
+  - [What is the CoperDBAPI?](#what-is-the-coperdbapi)
+    - [Example Usage](#example-usage)
+    - [ProducerWave](#producerwave)
+    - [ProducerWind](#producerwind)
+    - [API](#api)
+      - [Request](#request)
+      - [Response](#response)
 
-### What is the CoperDBAPI?
+## What is the CoperDBAPI?
 
-
+Lorem impsum dolor sit amet, consectet
 
 ---
 
-#### Example Usage
+### Example Usage
 
+In the same folder with docker-compose.yml
 
-###### In the same folder with docker-compose.yml
-```shell
+```sh
 docker-compose up --build
 ```
 
-###### If you want to be sure for orphans containers you can do
-```shell
+To ensure there are no orphan containers, you can use
+
+```sh
 docker-compose up --build --remove-orphans
 ```
-<p align="center">
-  <img width="700" height="600" src="https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-24%20at%205.32.18%20PM.png" alt="Sublime's custom image"/>
-</p>
 
-###### Keep docker compose running
-
+![Sublime's custom image](https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-24%20at%205.32.18%20PM.png)
 
 ### ProducerWave
 
-The first time it will pull data from the Copernicus is when it is first uploaded the docker composes.
-After from that it will take data every 3 hours.
-Duplicates don't exist because the time that pull data from Copernicus is :
-```code
-current_time - 3hours + 1second until current time
-```
+The first time it will pull data from Copernicus is when the docker compose is first uploaded. After that, it will retrieve data every 3 hours. Duplicates do not exist because the time range for pulling data from Copernicus is: `current_time - 3 hours + 1 second`
 
-Copernicus has information every 3hours started at 00.00
-```example
-If the program starts at 05.00 o'clock that means that the first time that gets is 5 - (5%2) = 3 --> 03.00 o'clock
-```
+Copernicus updates information every 3 hours starting at 00:00. If the program starts at 05:00 o'clock, that means that the first time it retrieves data is at: `5 - (5%3) = 2` --> 02:00 o'clock.
 
-We get the information as a .nc file from Copernicus, we refactor it into json and push it into kafka topic
-```example
-wave_topic
-```
-and into MongoDB into a collection named
+We obtain the information as a .nc file from Copernicus, refactor it into JSON, and push it into the Kafka topic `wave_topic` and into MongoDB in a collection named `waveData` with the following format:
 
-```example
-waveData
-```
-with the below format
-```
+```json
 {
   "time": "2023-07-24 12:00:00",
   "latitude": 35,
@@ -64,50 +51,39 @@ with the below format
   "vtm10": 3.4600000381469727
 }
 ```
-The information analyzes below:
-```
-Significant Wave Height (VHM0) (m) S.I.
-Wave Direction (VMDR)
-Wave period mean value (VTM10)
-```
 
-<p align="center">
-  <img width="400" height="200" src="https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-25%20at%208.24.36%20AM.png" alt="CoperDBAPI"/>
-</p>
+The information is analyzed below:
 
-The Horizontal Resolution is:
-```
-0.083° x 0.083°
-```
+| Variable | Description                  | Unit |
+|----------|------------------------------|------|
+| vhm0     | Significant Wave Height      |meters|
+| vmdr     | Wave Direction               |      |
+| vtm10    | Wave Period Mean Value       |      |
+
+![CoperDBAPI](https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-25%20at%208.24.36%20AM.png)
+
+The horizontal resolution is: `0.083° x 0.083°`
+
 ---
 
 ### ProducerWind
 
-The first time it will pull data from the Copernicus is when it is first uploaded the docker composes.
-After from that it will take data every 1 day.
-The erliest data that we can get from Copernicus is 6 days ago.
-The values of time in which we have access are the follows:
-```
+The first time it will pull data from Copernicus is when the docker compose is first uploaded. After that, it will retrieve data every day. The earliest data that we can get from Copernicus is from 6 days ago. The available time values are as follows:
+
+```json
 'time': [
-                    '00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
-                    '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
-                    '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
-                    '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
-                ]
+  '00:00', '01:00', '02:00', '03:00', '04:00', '05:00',
+  '06:00', '07:00', '08:00', '09:00', '10:00', '11:00',
+  '12:00', '13:00', '14:00', '15:00', '16:00', '17:00',
+  '18:00', '19:00', '20:00', '21:00', '22:00', '23:00',
+]
 ```
-Duplicates don't exist.
 
-We get the information as a .nc file from Copernicus, we refactor it into json and push it into kafka topic
-```example
-wind_topic
-```
-and into MongoDB into a collection named
+Duplicates do not exist.
 
-```example
-windData
-```
-with the below format
-```
+We obtain the information as a .nc file from Copernicus, refactor it into JSON, and push it into the Kafka topic `wind_topic` and into MongoDB in a collection named `wind_data` with the following format:
+
+```json
 {
   "time": "2023-07-18 00:00:00",
   "latitude": 50.150001525878906,
@@ -119,101 +95,69 @@ with the below format
 }
 ```
 
-The information analyzes below:
-```
-East wind component (u10) (m/s) S.I.
-North wind component (v10) (m/s) S.I.
-Speed is a combination of the above two components (speed) (m/s) S.I.
-```
+The information is analyzed below:
 
-Speed information :
-<p align="center">
-  <img width="300" height="250" src="https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-25%20at%208.25.32%20AM.png" alt="CoperDBAPI"/>
-</p>
+| Variable | Description                             | Unit  |
+|----------|-----------------------------------------|-------|
+| u10      | East Wind Component                     | m/s   |
+| v10      | North Wind Component                    | m/s   |
+| speed    | Combination of the above two components | m/s   |
 
-<p align="center">
-  <img width="150" height="50" src="https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-25%20at%208.25.44%20AM.png" alt="CoperDBAPI"/>
-</p>
+Speed information:
 
-The Horizontal Resolution is:
-```
-0.25° x 0.25°
-```
+![CoperDBAPI](https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-25%20at%208.25.32%20AM.png)
+
+![CoperDBAPI](https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-25%20at%208.25.44%20AM.png)
+
+The horizontal resolution is: `0.25° x 0.25°`
+
 ---
 
 ### API
 
-## Request
+#### Request
 
-`GET /data`
+`GET /data?dateMin=2023-07-19T04:00:00&dateMax=2023-07-19T07:00:00&latitude=35&longitude=18&radius=20`
 
-    http://127.0.0.1:5000/data?dateMin=2023-07-19T04:00:00&dateMax=2023-07-19T07:00:00&latitude=35&longitude=18&radius=20
+Users must provide 5 variables: `dateMin`, `dateMax`, `latitude`, `longitude`, `radius`.
 
-    User has to fill 5 variables : dateMin, dateMax, latitude, longitude, radius
+![CoperDBAPI](https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-25%20at%2011.53.06%20AM.png)
 
-<p align="center">
-  <img width="250" height="250" src="https://github.com/ArtemisStefanidou/CoperDBAPI/blob/main/photos/Screenshot%202023-07-25%20at%2011.53.06%20AM.png" alt="CoperDBAPI"/>
-</p>
-## Response
+#### Response
 
-When the user gives a date older or newer than those in the collections, returned an empty list.
+If the user provides a date older or newer than those in the collections, an empty list is returned.
 
-    [
+When a valid date is provided, we check if data exists for the specified latitude and longitude. If data exists, information from both collections is returned.
+
+```json
+[
+  {
+    "waveData": [
       {
-        "waveData": [
-          
-        ]
+        "time": "2023-07-19 06:00:00",
+        "latitude": 35,
+        "longitude": 18.916666666666657,
+        "vhm0": 0.25999999046325684,
+        "vmdr": 322.69000244140625,
+        "vtm10": 3.4600000381469727
       },
-      {
-        "windData": [
-          
-        ]
-      }
+      {...}
     ]
-    
-When the user gives a valid date we check if it has data for this langitude, longitude given by the user.
-If it has, it returns the information for both collections.
-
-    [
+  },
+  {
+    "windData": [
       {
-        "waveData": [
-          {
-            "time": "2023-07-19 06:00:00",
-            "latitude": 35,
-            "longitude": 18.916666666666657,
-            "vhm0": 0.25999999046325684,
-            "vmdr": 322.69000244140625,
-            "vtm10": 3.4600000381469727
-          }
-          {...}
-        ]
+        "time": "2023-07-19 06:00:00",
+        "latitude": 35,
+        "longitude": 18.916666666666657,
+        "vhm0": 0.25999999046325684,
+        "vmdr": 322.69000244140625,
+        "vtm10": 3.4600000381469727
       },
-      {
-        "windData": [
-          {
-            "time": "2023-07-19 06:00:00",
-            "latitude": 35,
-            "longitude": 18.916666666666657,
-            "vhm0": 0.25999999046325684,
-            "vmdr": 322.69000244140625,
-            "vtm10": 3.4600000381469727
-          }
-          {...}
-        ]
-      }
+      {...}
     ]
+  }
+]
+```
 
-If not, it returns an empty list.
-
-    [
-      {
-        "waveData": [
-          
-        ]
-      },
-      {
-        "windData": [
-          
-        ]
-      }
-    ]
+If not, an empty list is returned.
